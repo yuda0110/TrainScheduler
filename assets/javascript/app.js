89fc00163917f2af01467eed6fd6a255c19ecea0
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-22 20:41:11 -0700

    Add firebase scripts and the config code

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBip3ACe6D9SK0XRQWsA24RMedaqzuRUPs",
+  authDomain: "train-schedule-4d239.firebaseapp.com",
+  databaseURL: "https://train-schedule-4d239.firebaseio.com",
+  projectId: "train-schedule-4d239",
+  storageBucket: "train-schedule-4d239.appspot.com",
+  messagingSenderId: "340305875143",
+  appId: "1:340305875143:web:54ce8d647b9a4dd2fd6ee2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
