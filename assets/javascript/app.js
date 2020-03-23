27912c0bc537a884604875f1d3376f2cd08d807e
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-22 21:12:20 -0700

    Add click event to #add-train-btn in order to store the inputs' data in the firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,46 @@
+$(document).ready(function () {
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyBip3ACe6D9SK0XRQWsA24RMedaqzuRUPs",
-  authDomain: "train-schedule-4d239.firebaseapp.com",
-  databaseURL: "https://train-schedule-4d239.firebaseio.com",
-  projectId: "train-schedule-4d239",
-  storageBucket: "train-schedule-4d239.appspot.com",
-  messagingSenderId: "340305875143",
-  appId: "1:340305875143:web:54ce8d647b9a4dd2fd6ee2"
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyBip3ACe6D9SK0XRQWsA24RMedaqzuRUPs",
+    authDomain: "train-schedule-4d239.firebaseapp.com",
+    databaseURL: "https://train-schedule-4d239.firebaseio.com",
+    projectId: "train-schedule-4d239",
+    storageBucket: "train-schedule-4d239.appspot.com",
+    messagingSenderId: "340305875143",
+    appId: "1:340305875143:web:54ce8d647b9a4dd2fd6ee2"
+  };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
-const database = firebase.database();
+  const database = firebase.database();
+
+  const htmlEl = {
+    trainName: $('#train-name'),
+    destination: $('#destination'),
+    firstTrainTime: $('#first-train-time'),
+    frequency: $('#frequency')
+  };
+
+  const trainSchedule = {
+
+  };
+
+
+  $('#add-train-btn').on('click', function (e) {
+    e.preventDefault();
+
+    database.ref().push({
+      trainName: htmlEl.trainName.val().trim(),
+      destination: htmlEl.destination.val().trim(),
+      firstTrainTime: htmlEl.firstTrainTime.val().trim(),
+      frequency: parseInt(htmlEl.frequency.val().trim())
+    });
+
+    htmlEl.trainName.val('');
+    htmlEl.destination.val('');
+    htmlEl.firstTrainTime.val('');
+    htmlEl.frequency.val('');
+
+  });
+});
+
