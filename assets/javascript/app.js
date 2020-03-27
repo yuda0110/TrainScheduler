e249e224afb84f2778e9f914658607760704a5d7
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-27 14:49:36 -0700

    Create renderEditForm method and updateData method, and call updateData when update-train-btn gets clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,6 +52,50 @@ $(document).ready(function () {
       });
     },
 
+    getCurrentData: function (key) {
+      database.ref('/' + key).once('value').then(function(snapshot) {
+        const data = {};
+        const trainName = (snapshot.val() && snapshot.val().trainName) || 'Anonymous';
+        const destination = (snapshot.val() && snapshot.val().destination) || 'Anonymous';
+        const frequency = (snapshot.val() && snapshot.val().frequency) || '1';
+        const firstTrainTime = (snapshot.val() && snapshot.val().firstTrainTime) || '00:00';
+
+        data['trainName'] = trainName;
+        data['destination'] = destination;
+        data['frequency'] = frequency;
+        data['firstTrainTime'] = firstTrainTime;
+
+        console.log('getCurrentData======');
+        console.log(data);
+
+        return data;
+      });
+    },
+
+    updateData: function (key) {
+      console.log('key!!!!!: ' + key);
+
+      let firstTime = '00:00';
+
+      this.dataList.forEach(function (data) {
+        if (data.id === key) {
+          firstTime = data.firstTime;
+        }
+      });
+
+      const editedData = {
+        trainName: $('#new-train-name').val().trim(),
+        destination: $('#new-destination').val().trim(),
+        firstTrainTime: firstTime.trim(),
+        frequency: parseInt($('#new-frequency').val().trim())
+      };
+
+      const updates = {};
+      updates[`/${key}`] = editedData;
+
+      return database.ref().update(updates);
+    },
+
     renderTable: function (snapshot) {
       const key = snapshot.key;
       const snapshotVal = snapshot.val();
@@ -72,9 +116,9 @@ $(document).ready(function () {
       removeBtn.attr('data-key', key);
 
       tr.append(
-        $('<td>').text(snapshotVal.trainName),
-        $('<td>').text(snapshotVal.destination),
-        $('<td>').text(snapshotVal.frequency),
+        $('<td class="td__train-name">').text(snapshotVal.trainName),
+        $('<td class="td__destination">').text(snapshotVal.destination),
+        $('<td class="td__frequency">').text(snapshotVal.frequency),
         $(`<td id="next-arrival__${key}">`).text(nextArrival),
         $(`<td id="min-away__${key}">`).text(minTillTrain),
         $('<td>').append(updateBtn, removeBtn)
@@ -96,6 +140,35 @@ $(document).ready(function () {
 
     dbErrorLog: function (errorObject) {
       console.log("The read failed: " + errorObject.code);
+    },
+
+    renderEditForm: function (key) {
+      const trainNameVal = $(`#${key} .td__train-name`).text();
+      const destinationVal = $(`#${key} .td__destination`).text();
+      const frequencyVal = $(`#${key} .td__frequency`).text();
+
+      const darkBg = $('<div class="dark-bg">');
+
+      const editTrainEl = $('<section id="edit-train">');
+      const h3El = $('<h3>').text('Edit Train Schedule');
+      const formEl = $(`<form>`);
+      const formGroupEl1 = $('<div class="form-group">');
+      const trainNameLabel = $('<label for="train-name">').text('Train Name');
+      const trainNameInput = $(`<input id="new-train-name" type="text" value="${trainNameVal}" required>`);
+      $(formGroupEl1).append(trainNameLabel, trainNameInput);
+      const formGroupEl2 = $('<div class="form-group">');
+      const destinationLabel = $('<label for="destination">').text('Destination');
+      const destinationInput = $(`<input id="new-destination" type="text" value="${destinationVal}" required>`);
+      $(formGroupEl2).append(destinationLabel, destinationInput);
+      const formGroupEl3 = $('<div class="form-group">');
+      const frequencyLabel = $('<label for="frequency">').text('Frequency');
+      const frequencyInput = $(`<input id="new-frequency" type="text" value="${frequencyVal}" required>`);
+      $(formGroupEl3).append(frequencyLabel, frequencyInput);
+      const updateBtn = $(`<button id="update-train-btn" class="btn-form btn-submit" data-key="${key}">`).text('Update');
+      const cancelBtn = $('<button id="cancel-btn" class="btn-form btn-cancel">').text('Cancel');
+      $(formEl).append(formGroupEl1, formGroupEl2, formGroupEl3, updateBtn, cancelBtn);
+      $(editTrainEl).append(h3El, formEl);
+      $('body').append(darkBg, editTrainEl);
     },
 
     removeEditForm: function () {
@@ -129,34 +202,24 @@ $(document).ready(function () {
     trainSchedule.emptyAllInputs();
   });
 
+  $(document).on('click', '#update-train-btn', function (e) {
+    e.preventDefault();
+
+    trainSchedule.updateData($(this).attr('data-key'));
+    trainSchedule.removeEditForm();
+  });
+
   $(document).on('click', '.btn-remove', function() {
     database.ref().child($(this).attr('data-key')).remove();
   });
 
   $(document).on('click', '.btn-update', function() {
     // Render a form to update Train Name, Destination, and Frequency
-    const darkBg = $('<div class="dark-bg">');
 
-    const editTrainEl = $('<section id="edit-train">');
-    const h3El = $('<h3>').text('Edit Train Schedule');
-    const formEl = $('<form>');
-    const formGroupEl1 = $('<div class="form-group">');
-    const trainNameLabel = $('<label for="train-name">').text('Train Name');
-    const trainNameInput = $('<input id="train-name" type="text" required>');
-    $(formGroupEl1).append(trainNameLabel, trainNameInput);
-    const formGroupEl2 = $('<div class="form-group">');
-    const destinationLabel = $('<label for="destination">').text('Destination');
-    const destinationInput = $('<input id="destination" type="text" required>');
-    $(formGroupEl2).append(destinationLabel, destinationInput);
-    const formGroupEl3 = $('<div class="form-group">');
-    const frequencyLabel = $('<label for="frequency">').text('Frequency');
-    const frequencyInput = $('<input id="frequency" type="text" required>');
-    $(formGroupEl3).append(frequencyLabel, frequencyInput);
-    const updateBtn = $('<button id="update-train-btn" class="btn-form btn-submit">').text('Update');
-    const cancelBtn = $('<button id="cancel-btn" class="btn-form btn-cancel">').text('Cancel');
-    $(formEl).append(formGroupEl1, formGroupEl2, formGroupEl3, updateBtn, cancelBtn);
-    $(editTrainEl).append(h3El, formEl);
-    $('body').append(darkBg, editTrainEl);
+    const key = $(this).attr('data-key');
+    console.log('btn-update key:' + key);
+
+    trainSchedule.renderEditForm(key);
   });
 
   $(document).on('click', '.dark-bg', function () {
